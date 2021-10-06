a0a9b47ebbb39c2e9c6fee21a37edb3fc119a378
Adolfo Diego <yakshim0@gmail.com>
2021-10-06 02:43:14 -0600

    Agregar nueva publicacion y editar publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from typing import ValuesView
 from django.urls import path
 from django.urls import path
 from . import views
@@ -5,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
     path('publicacion/<int:pk>', views.publicacion_detalle, name='publicacion_detalle'),
+    path('publicacion/nueva/', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
 ]
