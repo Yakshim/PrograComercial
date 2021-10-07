ab69ab42badf209775671262ca57216cbe67e66e
Adolfo Diego <yakshim0@gmail.com>
2021-10-07 02:22:52 -0600

    Agregando el borrador, eliminacion y el login

--- blog/views.py
+++ blog/views.py
@@ -3,17 +3,19 @@ from django.utils import timezone
 from .models import Publicacion
 from .forms import PublicacionForm
 from blog import forms
+from django.contrib.auth.decorators import login_required
 
 
 def publicacion_lista(request):
     publicaciones= Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/publicacion_lista2.html', {'publicaciones': publicaciones})
 
-
+@login_required
 def publicacion_detalle(request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/publicacion_detalle.html',{'publicacion':publicacion})
 
+@login_required
 def publicacion_nueva(request):
     if request.method == "POST":
         formulario = PublicacionForm(request.POST)
@@ -27,6 +29,8 @@ def publicacion_nueva(request):
         formulario = PublicacionForm()        
     return render(request, 'blog/publicacion_editar.html', {'formulario': formulario})
 
+
+@login_required
 def publicacion_editar(request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     if request.method == "POST":
@@ -41,4 +45,21 @@ def publicacion_editar(request, pk):
         formulario = PublicacionForm(instance=publicacion)
     return render(request, 'blog/publicacion_editar.html', {'formulario': formulario})
 
+
+@login_required
+def publicacion_borrador_lista(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/publicacion_borrador_lista.html', {'publicaciones': publicaciones})
+
+@login_required
+def publicacion_publicar(request, pk):
+    publicacion= get_object_or_404(Publicacion, pk=pk)
+    publicacion.publicar()
+    return redirect('publicacion_detalle', pk=pk)
+
+@login_required
+def publicacion_eliminar(request, pk):
+    publicacion= get_object_or_404(Publicacion, pk=pk)
+    publicacion.delete()
+    return redirect('publicacion_lista')
 # Create your views here.
--- misitio/urls.py
+++ misitio/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from blog import views
+
+
+from django.urls import include, path
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(template_name='blog/registration/login.html'), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls'))
 ]
